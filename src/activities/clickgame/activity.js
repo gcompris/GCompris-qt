893dcc660436caaa978735d686a75f1b9a70a9ed
Bruno Coudoin <bruno.coudoin@gcompris.net>
2014-01-25 17:52:14 +0100

    clickgame, removed border around fishes.

--- src/activities/clickgame/activity.js
+++ src/activities/clickgame/activity.js
@@ -5,164 +5,164 @@ var fishes = [
             {
               "imgName": "blueking2.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 118,
+              "height": 76
             },
             {
               "imgName": "butfish.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 98,
+              "height": 82
             },
             {
               "imgName": "cichlid1.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 63,
+              "height": 37
             },
             {
               "imgName": "cichlid4.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 73,
+              "height": 44
             },
             {
               "imgName": "collaris.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 62,
+              "height": 50
             },
             {
               "imgName": "discus2.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 100,
+              "height": 100
             },
             {
               "imgName": "discus3.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 63,
+              "height": 57
             },
             {
               "imgName": "f00.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 64,
+              "height": 59
             },
             {
               "imgName": "f01.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 63,
+              "height": 50
             },
             {
               "imgName": "f02.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 71,
+              "height": 44
             },
             {
               "imgName": "f03.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 83,
+              "height": 52
             },
             {
               "imgName": "f04.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 93,
+              "height": 62
             },
             {
               "imgName": "f05.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 90,
+              "height": 49
             },
             {
               "imgName": "f06.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 93,
+              "height": 44
             },
             {
               "imgName": "f07.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 82,
+              "height": 75
             },
             {
               "imgName": "f08.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 91,
+              "height": 70
             },
             {
               "imgName": "f09.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 108,
+              "height": 86
             },
             {
               "imgName": "f10.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 97,
+              "height": 60
             },
             {
               "imgName": "f11.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 116,
+              "height": 42
             },
             {
               "imgName": "f12.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 100,
+              "height": 35
             },
             {
               "imgName": "f13.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 116,
+              "height": 73
             },
             {
               "imgName": "manta.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 266,
+              "height": 91
             },
             {
               "imgName": "newf1.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 118,
+              "height": 81
             },
             {
               "imgName": "QueenAngel.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 78,
+              "height": 54
             },
             {
               "imgName": "shark1.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 187,
+              "height": 53
             },
             {
               "imgName": "six_barred.png",
               "nbFrame": 2,
-              "width": 128,
-              "height": 126
+              "width": 76,
+              "height": 63
             },
             {
               "imgName": "teeth.png",
               "nbFrame": 8,
-              "width": 128,
-              "height": 126
+              "width": 64,
+              "height": 64
             },
         ]
 
