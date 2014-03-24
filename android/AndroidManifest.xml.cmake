a377c59048cda767ea3bdf4560a543046ff99ed6
Johnny Jazeix <jazeix@gmail.com>
2014-03-24 09:10:19 +0100

    Add a tool to convert pri to CMakeFiles and add all android resources files

--- android/AndroidManifest.xml.cmake
+++ android/AndroidManifest.xml.cmake
@@ -12,20 +12,19 @@
 			<meta-data android:name="android.app.qt_libs_resource_id" android:resource="@array/qt_libs"/>
 			<meta-data android:name="android.app.bundled_libs_resource_id" android:resource="@array/bundled_libs"/>
 			<!-- Deploy Qt libs as part of package -->
-			<meta-data android:name="android.app.bundle_local_qt_libs" android:value="-- %%BUNDLE_LOCAL_QT_LIBS%% --"/>
+			<meta-data android:name="android.app.bundle_local_qt_libs" android:value="1"/>
 			<meta-data android:name="android.app.bundled_in_lib_resource_id" android:resource="@array/bundled_in_lib"/>
 			<meta-data android:name="android.app.bundled_in_assets_resource_id" android:resource="@array/bundled_in_assets"/>
 			<!-- Run with local libs -->
-			<meta-data android:name="android.app.use_local_qt_libs" android:value="-- %%USE_LOCAL_QT_LIBS%% --"/>
+			<meta-data android:name="android.app.use_local_qt_libs" android:value="1"/>
 			<meta-data android:name="android.app.libs_prefix" android:value="/data/local/tmp/qt/"/>
-			<meta-data android:name="android.app.load_local_libs" android:value="-- %%INSERT_LOCAL_LIBS%% --"/>
-			<meta-data android:name="android.app.load_local_jars" android:value="-- %%INSERT_LOCAL_JARS%% --"/>
-			<meta-data android:name="android.app.static_init_classes" android:value="-- %%INSERT_INIT_CLASSES%% --"/>
+			<meta-data android:name="android.app.load_local_libs" android:value="plugins/platforms/android/libqtforandroidGL.so:lib/libQt5QuickParticles.so:lib/libQt5Xml.so:plugins/mediaservice/libqtmedia_android.so:lib/libQt5MultimediaQuick_p.so"/>
+			<meta-data android:name="android.app.load_local_jars" android:value="jar/QtAndroid.jar:jar/QtAndroidAccessibility.jar:jar/QtAndroid-bundled.jar:jar/QtAndroidAccessibility-bundled.jar:jar/QtMultimedia.jar:jar/QtMultimedia-bundled.jar"/>
+			<meta-data android:name="android.app.static_init_classes" android:value="org.qtproject.qt5.android.multimedia.QtMultimediaUtils:org.qtproject.qt5.android.multimedia.QtMultimediaUtils"/>
 			<!--  Messages maps -->
 			<meta-data android:name="android.app.ministro_not_found_msg" android:value="@string/ministro_not_found_msg"/>
 			<meta-data android:name="android.app.ministro_needed_msg" android:value="@string/ministro_needed_msg"/>
 			<meta-data android:name="android.app.fatal_error_msg" android:value="@string/fatal_error_msg"/>
-			<!--  Messages maps -->
 		</activity>
 	</application>
 	<supports-screens android:normalScreens="true" android:anyDensity="true" android:smallScreens="true" android:largeScreens="true"/>
