00ef4f4901935aaddbcbc64695cde03940b47ba0
Johnny Jazeix <jazeix@gmail.com>
2014-04-04 19:08:32 +0200

    Remove all generated files for the apk generation. Use a modified version of androiddeployqt to create the apk (for copying Qt resources).

--- android/AndroidManifest.xml.cmake
+++ android/AndroidManifest.xml.cmake
@@ -1,26 +1,26 @@
 <?xml version='1.0' encoding='utf-8'?>
 <manifest android:installLocation="auto" android:versionName="@ANDROID_VERSION_NAME@" xmlns:android="http://schemas.android.com/apk/res/android" package="net.gcompris" android:versionCode="6">
-	<application android:label="GCompris" android:name="org.qtproject.qt5.android.bindings.QtApplication" android:icon="@drawable/icon">
+	<application android:label="@string/app_name" android:name="org.qtproject.qt5.android.bindings.QtApplication" android:icon="@drawable/icon">
 		<activity android:configChanges="orientation|uiMode|screenLayout|screenSize|smallestScreenSize|locale|fontScale|keyboard|keyboardHidden|navigation" android:label="@string/app_name" android:name="org.qtproject.qt5.android.bindings.QtActivity" android:screenOrientation="unspecified">
 			<intent-filter>
 				<action android:name="android.intent.action.MAIN"/>
 				<category android:name="android.intent.category.LAUNCHER"/>
 			</intent-filter>
-			<meta-data android:name="android.app.lib_name" android:value="GCompris"/>
+			<meta-data android:name="android.app.lib_name" android:value="-- %%INSERT_APP_LIB_NAME%% --"/>
 			<meta-data android:name="android.app.qt_sources_resource_id" android:resource="@array/qt_sources"/>
 			<meta-data android:name="android.app.repository" android:value="default"/>
 			<meta-data android:name="android.app.qt_libs_resource_id" android:resource="@array/qt_libs"/>
 			<meta-data android:name="android.app.bundled_libs_resource_id" android:resource="@array/bundled_libs"/>
 			<!-- Deploy Qt libs as part of package -->
-			<meta-data android:name="android.app.bundle_local_qt_libs" android:value="1"/>
+			<meta-data android:name="android.app.bundle_local_qt_libs" android:value="-- %%BUNDLE_LOCAL_QT_LIBS%% --"/>
 			<meta-data android:name="android.app.bundled_in_lib_resource_id" android:resource="@array/bundled_in_lib"/>
 			<meta-data android:name="android.app.bundled_in_assets_resource_id" android:resource="@array/bundled_in_assets"/>
 			<!-- Run with local libs -->
-			<meta-data android:name="android.app.use_local_qt_libs" android:value="1"/>
+			<meta-data android:name="android.app.use_local_qt_libs" android:value="-- %%USE_LOCAL_QT_LIBS%% --"/>
 			<meta-data android:name="android.app.libs_prefix" android:value="/data/local/tmp/qt/"/>
-			<meta-data android:name="android.app.load_local_libs" android:value="plugins/platforms/android/libqtforandroidGL.so:lib/libQt5QuickParticles.so:lib/libQt5Xml.so:plugins/mediaservice/libqtmedia_android.so:lib/libQt5MultimediaQuick_p.so"/>
-			<meta-data android:name="android.app.load_local_jars" android:value="jar/QtAndroid.jar:jar/QtAndroidAccessibility.jar:jar/QtAndroid-bundled.jar:jar/QtAndroidAccessibility-bundled.jar:jar/QtMultimedia.jar:jar/QtMultimedia-bundled.jar"/>
-			<meta-data android:name="android.app.static_init_classes" android:value="org.qtproject.qt5.android.multimedia.QtMultimediaUtils:org.qtproject.qt5.android.multimedia.QtMultimediaUtils"/>
+			<meta-data android:name="android.app.load_local_libs" android:value="-- %%INSERT_LOCAL_LIBS%% --"/>
+			<meta-data android:name="android.app.load_local_jars" android:value="-- %%INSERT_LOCAL_JARS%% --"/>
+			<meta-data android:name="android.app.static_init_classes" android:value="-- %%INSERT_INIT_CLASSES%% --"/>
 			<!--  Messages maps -->
 			<meta-data android:name="android.app.ministro_not_found_msg" android:value="@string/ministro_not_found_msg"/>
 			<meta-data android:name="android.app.ministro_needed_msg" android:value="@string/ministro_needed_msg"/>
---
+++ android/configAndroid.json.cmake
@@ -0,0 +1,13 @@
+{
+ "description": "This file is to be read by androiddeployqt",
+ "qt": "@_qt5Core_install_prefix@",
+ "sdk": "@ANDROID_SDK@",
+ "ndk": "@ANDROID_NDK@",
+ "toolchain-prefix": "@ANDROID_TOOLCHAIN_MACHINE_NAME@",
+ "tool-prefix": "@ANDROID_TOOLCHAIN_MACHINE_NAME@",
+ "toolchain-version": "@ANDROID_COMPILER_VERSION@",
+ "ndk-host": "@ANDROID_NDK_HOST_SYSTEM_NAME@",
+ "target-architecture": "@ARM_TARGET@",
+ "application-binary": "@CMAKE_LIBRARY_OUTPUT_DIRECTORY@libGCompris.so",
+ "android-package": "net.gcompris"
+}
