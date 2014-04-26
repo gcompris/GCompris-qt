2616435acf4ec2e7aabbec3b002fa0a82c656895
Bruno Coudoin <bruno.coudoin@gcompris.net>
2014-04-27 01:21:09 +0200

    convert tool, no more overwrite already translated strings
    This makes it possible to run the tool several times once
    new strings are added in the Qt Quick version while not
    overwriting edited strings in the ts file.

--- tools/convertPoToTsFiles/convertPo.py
+++ tools/convertPoToTsFiles/convertPo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Using polib.py from https://bitbucket.org/izi/polib/wiki/Home
 
 # Usage : python convertPo.py input1.po input2.ts output.ts
@@ -11,12 +12,12 @@ import sys
 import xml.etree.ElementTree as ET
 
 if len(sys.argv) < 4:
-    print('Usage : python convertPo.py gcompris_gtk.po gcompris_qt_template.ts $LOCALE. Output will be gcompris_$LOCALE.ts ($LOCALE is fr_FR for example).')
+    print('Usage : python convertPo.py gcompris_gtk.po gcompris_qt_template.ts $LOCALE.'
+          ' Output will be gcompris_$LOCALE.ts ($LOCALE is fr_FR for example).')
     sys.exit(1)
 
 # load an existing po file
 po = polib.pofile(sys.argv[1])
-
 tree = ET.parse(sys.argv[2])
 root = tree.getroot()
 
@@ -26,11 +27,12 @@ root.set('language', sys.argv[3])
 for messages in root.iter('message'):
     original = messages.find('source').text
     if original != None:
-      for entry in po:
-        if entry.msgid.encode('utf-8') == original.encode('utf-8'):
-          messages.find('translation').text = entry.msgstr
-          messages.find('translation').attrib.pop('type')
-          break
+        for entry in po:
+            if entry.msgid.encode('utf-8') == original.encode('utf-8') and \
+                    messages.find('translation').attrib['type'] == 'unfinished':
+                messages.find('translation').text = entry.msgstr
+                messages.find('translation').attrib.pop('type')
+                break
 
 with open('gcompris_' + sys.argv[3] + '.ts', 'wb') as f:
     f.write('<?xml version="1.0" encoding="utf-8" ?>\n<!DOCTYPE TS>\n'.encode('utf-8'))
