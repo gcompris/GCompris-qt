301ff77d4684c476292f69d9c18285961fb7301a
Stephane Mankowski <stephane@mankowski.fr>
2014-05-04 16:56:47 +0200

    clockgame-correction in comments

--- src/activities/clockgame/clockgame.js
+++ src/activities/clockgame/clockgame.js
@@ -1,24 +1,24 @@
 /* GCompris - clockgame.js
-*
-* Copyright (C) 2014 <YOUR NAME HERE>
-*
-* Authors:
-*   <THE GTK VERSION AUTHOR> (GTK+ version)
-*   "YOUR NAME" <YOUR EMAIL> (Qt Quick port)
-*
-*   This program is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
+ *
+ * Copyright (C) 2014 Stephane Mankowski <stephane@mankowski.fr>
+ *
+ * Authors:
+ *   Bruno Coudoin <bruno.coudoin@gcompris.net> (GTK+ version)
+ *   Stephane Mankowski <stephane@mankowski.fr> (Qt Quick port)
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 var url = "qrc:/gcompris/src/activities/clockgame/resource/"
 var currentLevel = 0
 var numberOfLevel = 10
