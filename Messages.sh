0fe125dc9dca1c2bb8c8313c11e77000d695cd25
Bruno Coudoin <bruno.coudoin@gcompris.net>
2014-11-11 23:01:03 +0100

    Messages, added info that we have utf8 strings
    Not sure why but some users require this option but not all.

--- Messages.sh
+++ Messages.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-$XGETTEXT `find src -name '*.qml' \
+$XGETTEXT --from-code utf-8 `find src -name '*.qml' \
            -o -name '*.js'` -cTRANSLATORS -kqsTr -L Java -o $podir/gcompris_qt.pot
 
-echo $XGETTEXT
-echo $XGETTEXT_QT
 
-#$XGETTEXT `find src -name '*.qml'` -kqsTr:1c,2,2t -L Java -o $podir/gcompris_qt.pot
-#$XGETTEXT src/activities/ballcatch/Ballcatch.qml  -kqsTr -L Java -o $podir/gcompris_qt.pot
