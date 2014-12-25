c5f26f7e677ca8b70dfa7cf7598780350938565c
Bruno Coudoin <bruno.coudoin@gcompris.net>
2014-12-25 20:26:50 +0100

    algebra_by, add a sound effect

--- src/activities/algebra_by/algebra.js
+++ src/activities/algebra_by/algebra.js
@@ -140,6 +140,7 @@ function questionsLeft() {
         otheritems.numpad.answerFlag = true
 
         if(coreItems.score.currentSubLevel < 10) {
+            coreItems.audioEffects.play("qrc:/gcompris/src/core/resource/sounds/flip.wav")
             coreItems.score.currentSubLevel++
             coreItems.timer.start()
         } else if(coreItems.score.currentSubLevel >= 10) {
