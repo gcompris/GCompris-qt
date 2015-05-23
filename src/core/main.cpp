a021772ef170bb3a9489f0cfd9d544eb7a8ce43e
Bruno Coudoin <bruno.coudoin@gcompris.net>
2015-05-23 23:46:44 +0200

    core, move translations init before the command line parsing
    We need translation to be initialized before the command line help.

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -58,6 +58,39 @@ bool loadAndroidTranslation(QTranslator &translator, const QString &locale)
     return true;
 }
 
+// Return the locale
+QString loadTranslation(QSettings &config, QTranslator &translator)
+{
+    QString locale;
+    // Get locale
+    if(config.contains("General/locale")) {
+        locale = config.value("General/locale").toString();
+    } else {
+        locale = GC_DEFAULT_LOCALE;
+    }
+    if(locale == GC_DEFAULT_LOCALE)
+        locale = QString(QLocale::system().name() + ".UTF-8");
+
+    if(locale == "C.UTF-8")
+        locale = "en_US.UTF-8";
+
+    // Load translation
+    // Remove .UTF8
+    locale.remove(".UTF-8");
+
+#if defined(Q_OS_ANDROID)
+    if(!loadAndroidTranslation(translator, locale))
+        loadAndroidTranslation(translator, ApplicationInfo::localeShort(locale));
+#else
+
+    if(!translator.load("gcompris_" + locale, QString("%1/%2/translations").arg(QCoreApplication::applicationDirPath(), GCOMPRIS_DATA_FOLDER))) {
+        qDebug() << "Unable to load translation for locale " <<
+                    locale << ", use en_US by default";
+    }
+#endif
+    return locale;
+}
+
 int main(int argc, char *argv[])
 {
 	QGuiApplication app(argc, argv);
@@ -65,6 +98,17 @@ int main(int argc, char *argv[])
     app.setApplicationName(GCOMPRIS_APPLICATION_NAME);
     app.setOrganizationDomain("kde.org");
     app.setApplicationVersion(ApplicationInfo::GCVersion());
+
+    // Local scope for config
+    QSettings config(QStandardPaths::writableLocation(QStandardPaths::GenericConfigLocation) +
+                     "/gcompris/" + GCOMPRIS_APPLICATION_NAME + ".conf",
+                     QSettings::IniFormat);
+
+    // Load translations
+    QTranslator translator;
+    QString locale = loadTranslation(config, translator);
+    // Apply translation
+    app.installTranslator(&translator);
 
     QCommandLineParser parser;
     parser.setApplicationDescription("GCompris is an educational software for children 2 to 10");
@@ -100,26 +144,9 @@ int main(int argc, char *argv[])
     // async callback from the payment system
     ApplicationSettings::getInstance()->checkPayment();
 
-    // Load configuration
-    QString locale;
     // Getting fullscreen mode from config if exist, else true is default value
     bool isFullscreen = true;
     {
-        // Local scope for config
-        QSettings config(QStandardPaths::writableLocation(QStandardPaths::GenericConfigLocation) +
-                         "/gcompris/" + GCOMPRIS_APPLICATION_NAME + ".conf",
-                         QSettings::IniFormat);
-        // Get locale
-        if(config.contains("General/locale")) {
-            locale = config.value("General/locale").toString();
-        } else {
-            locale = GC_DEFAULT_LOCALE;
-        }
-        if(locale == GC_DEFAULT_LOCALE)
-            locale = QString(QLocale::system().name() + ".UTF-8");
-
-        if(locale == "C.UTF-8")
-            locale = "en_US.UTF-8";
 
         if(config.contains("General/fullscreen")) {
             isFullscreen = config.value("General/fullscreen").toBool();
@@ -143,26 +170,6 @@ int main(int argc, char *argv[])
 		if(noCursor || parser.isSet(clNoCursor))
 			QGuiApplication::setOverrideCursor(QCursor(Qt::BlankCursor));
     }
-
-    // Load translation
-    // Remove .UTF8
-    locale.remove(".UTF-8");
-    // Look for a translation using this
-    QTranslator translator;
-
-#if defined(Q_OS_ANDROID)
-    if(!loadAndroidTranslation(translator, locale))
-        loadAndroidTranslation(translator, ApplicationInfo::localeShort(locale));
-#else
-
-    if(!translator.load("gcompris_" + locale, QString("%1/%2/translations").arg(QCoreApplication::applicationDirPath(), GCOMPRIS_DATA_FOLDER))) {
-        qDebug() << "Unable to load translation for locale " <<
-                    locale << ", use en_US by default";
-    }
-#endif
-
-    // Apply translation
-    app.installTranslator(&translator);
 
     // Update execution counter
     ApplicationSettings::getInstance()->setExeCount(ApplicationSettings::getInstance()->exeCount() + 1);
