af08fbed24ecaf95dfc3b31e6005561905fa6788
Bruno Coudoin <bruno.coudoin@gcompris.net>
2015-11-26 23:21:18 +0100

    explore_world_animals, fixed positioning

--- src/activities/explore_world_animals/explore_world_animals.js
+++ src/activities/explore_world_animals/explore_world_animals.js
@@ -20,62 +20,61 @@
 *   along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
-.import GCompris 1.0 as GCompris
 var tab = [
     {
-        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.png",
+        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.svg",
         "title": qsTr("Jaguar"),
         "text": qsTr("Jaguars are named after the Native American word meaning 'he who kills with one leap' because they like to climb trees to attack their prey."),
         "image2": "qrc:/gcompris/src/activities/explore_world_animals/resource/jaggy.jpg",
         "text2": qsTr("Jaguar"),
         "x": 0.36,
-        "y": 0.5,
-        "width": 0.2,
-        "height": 0.2
+        "y": 0.56,
+        "width": 0.0785,
+        "height": 0.1005
     },
     {
-        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.png",
+        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.svg",
         "title": qsTr("Hedgehog"),
         "text": qsTr("Hedgehogs eat small animals, like frogs and insects, so many people keep them as useful pets. When in danger, they will curl up into a ball and stick up their coat of sharp spines."),
         "image2": "qrc:/gcompris/src/activities/explore_world_animals/resource/hedgy.jpg",
         "text2": qsTr("Hedgehog"),
-        "x": 0.475,
-        "y": 0.136,
-        "width": 0.2,
-        "height": 0.2
+        "x": 0.53,
+        "y": 0.24,
+        "width": 0.0785,
+        "height": 0.1005
     },
     {
-        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.png",
+        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.svg",
         "title": qsTr("Giraffe"),
         "text": qsTr("The giraffe lives in Africa and is the tallest mammal in the world. Just their legs, which are usually 1.8 meters long, are taller than most humans!"),
         "image2": "qrc:/gcompris/src/activities/explore_world_animals/resource/giraffe.jpg",
         "text2": qsTr("Giraffe"),
-        "x": 0.527,
-        "y": 0.488,
-        "width": 0.2,
-        "height": 0.2
+        "x": 0.58,
+        "y": 0.48,
+        "width": 0.0785,
+        "height": 0.1005
     },
     {
-        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.png",
+        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.svg",
         "title": qsTr("Bison"),
         "text": qsTr("Bison live on the plains of North America and were hunted by the Native Americans for food."),
         "image2": "qrc:/gcompris/src/activities/explore_world_animals/resource/bison.jpg",
         "text2": qsTr("Bison"),
-        "x": 0.1875,
-        "y": 0.173,
-        "width": 0.2,
-        "height": 0.2
+        "x": 0.27,
+        "y": 0.29,
+        "width": 0.0785,
+        "height": 0.1005
     },
     {
-        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.png",
+        "image": "qrc:/gcompris/src/activities/explore_world_animals/resource/tux.svg",
         "title": qsTr("Narwhal"),
         "text": qsTr("Narwhals are whales that live in the Arctic Ocean and have long tusks. These tusks remind many people of the mythical unicorn's horn."),
         "image2": "qrc:/gcompris/src/activities/explore_world_animals/resource/narwhal.jpg",
         "text2": qsTr("Narwhal"),
-        "x": 0.442,
-        "y": 0.0,
-        "width": 0.2,
-        "height": 0.2
+        "x": 0.50,
+        "y": 0.06,
+        "width": 0.0785,
+        "height": 0.1005
     }
 ]
 
