gcompris/GCompris-qt

commit 1dce0d3540f2c7893b8b6db5a64df219c8a8c14d

build macosx, a plugin dir detection that works on macosx
It makes 'make package' to work

cmake/qt_helper.cmake
@@ -27,16 +27,25 @@ endfunction()
 #
 # The qml/-dir is always a sibling of the plugins/ dir.
 function(getQtPluginsPath _path)
-  get_target_property(_lib_file Qt5::Qml LOCATION)
-  get_filename_component(_lib_dir ${_lib_file} PATH)
-  # try $_lib_dir/qt5/plugins (Debian/Opensuse)
-  set(_plugin_root ${_lib_dir}/qt5/plugins)
-  if (NOT IS_DIRECTORY ${_plugin_root})
-    # try $_lib_dir/../plugins (Qt installer)
-    get_filename_component(_lib_dir_base ${_lib_dir} PATH)
-    set(_plugin_root ${_lib_dir_base}/plugins)
+  if(APPLE)
+    get_target_property(_lib Qt5::QJpegPlugin LOCATION)
+    get_filename_component(_lib ${_lib} DIRECTORY)
+    get_filename_component(_plugin_root ${_lib}/.. ABSOLUTE)
+    set(${_path} ${_plugin_root} PARENT_SCOPE)
+  else()
+    get_target_property(_lib_file Qt5::Qml LOCATION)
+    get_filename_component(_lib_dir ${_lib_file} PATH)
+    # try $_lib_dir/qt5/plugins (Debian/Opensuse)
+    set(_plugin_root ${_lib_dir}/qt5/plugins)
+    if (NOT IS_DIRECTORY ${_plugin_root})
+      # try $_lib_dir/../plugins (Qt installer)
+      get_filename_component(_lib_dir_base ${_lib_dir} PATH)
+      set(_plugin_root ${_lib_dir_base}/plugins)
+    endif()
+    get_target_property(_lib Qt5::QJpegPlugin LOCATION)
+    get_filename_component(_lib ${_lib} DIRECTORY)
+    get_filename_component(_plugin_root ${_lib}/.. ABSOLUTE)
   endif()
-  set(${_path} ${_plugin_root} PARENT_SCOPE)
 endfunction()
 
 function(getQtQmlPath _path)

commit b610e83a8a080e0b191faacf7c2cc6b66bec4622

build: fix build on linux
Regression from 1dce0d3

cmake/qt_helper.cmake
@@ -31,7 +31,6 @@ function(getQtPluginsPath _path)
     get_target_property(_lib Qt5::QJpegPlugin LOCATION)
     get_filename_component(_lib ${_lib} DIRECTORY)
     get_filename_component(_plugin_root ${_lib}/.. ABSOLUTE)
-    set(${_path} ${_plugin_root} PARENT_SCOPE)
   else()
     get_target_property(_lib_file Qt5::Qml LOCATION)
     get_filename_component(_lib_dir ${_lib_file} PATH)
@@ -46,6 +45,7 @@ function(getQtPluginsPath _path)
     get_filename_component(_lib ${_lib} DIRECTORY)
     get_filename_component(_plugin_root ${_lib}/.. ABSOLUTE)
   endif()
+  set(${_path} ${_plugin_root} PARENT_SCOPE)
 endfunction()
 
 function(getQtQmlPath _path)