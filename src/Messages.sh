542f2343184c1256b1afe66f2a43e063522e2faa
Johnny Jazeix <jazeix@gmail.com>
2017-05-18 10:23:24 +0200

    voices, create a po file to track the text/voice coherency

--- src/Messages.sh
+++ src/Messages.sh
@@ -4,3 +4,7 @@
 # EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
 # lconvert.
 $EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.qml -o -name \*.js` -o $podir/gcompris_qt.pot
+
+# create a pot for the voices text to keep them up-to-date with the audio files
+grep -R "intro: " --include="ActivityInfo.qml" | awk -F'[:/]' '{print "//i18n: intro voices, see GCompris-voices/README.md - activity: \""$2"\"\ni18n("$7");"}' | $XGETTEXT - -o $podir/gcompris_voices.pot
+
