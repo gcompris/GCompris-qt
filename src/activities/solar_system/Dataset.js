7360536d6200dd0b0ccdcd6bd4fcdcbf946ffe57
Aman Kumar Gupta <gupta2140@gmail.com>
2017-11-10 21:47:14 +0530

    Updated dataset: added questions for Sun and prevented closeness and other numeric values from being translated

--- src/activities/solar_system/Dataset.js
+++ src/activities/solar_system/Dataset.js
@@ -5,29 +5,29 @@ function get() {
 			"realImg": "qrc:/gcompris/src/activities/solar_system/resource/sun_real.svg",
 			"levels": [
 				{ //sub-level 1
-					"question": qsTr("It takes _____ for the Sun’s light to reach the earth"),
-					"options": [qsTr("8 minutes"),qsTr("30 minutes"),qsTr("60 minutes"),qsTr("15 minutes")],
-					"closeness": [qsTr("100%"),qsTr("58.1%%"),qsTr("1%"),qsTr("86.6%")]
+					"question": qsTr("The sun is the_____ largest object in our solar system"),
+					"options": [qsTr("Sixth"),qsTr("Third"),qsTr("First"),qsTr("Seventh")],
+					"closeness": ["17.5%","67%","100%","1%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("The temperature of the sun is around _____degrees"),
-					"options": [qsTr("1000"),qsTr("4500"),qsTr("5505"),qsTr("3638")],
-					"closeness": [qsTr("1%"),qsTr("78%"),qsTr("100%"),qsTr("60%")]
+					"options": [1000,4500,5505,3638],
+					"closeness": ["1%","78%","100%","60%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("Sun is ___ years old"),
 					"options": [qsTr("1.2 billion"),qsTr("3 billion"),qsTr("7 billion"),qsTr("4.5 billion")],
-					"closeness": [qsTr("1%"),qsTr("55%"),qsTr("25%"),qsTr("100%")]
+					"closeness": ["1%","55%","25%","100%"]
 				},
 				{ //sub-level 4
-					"question": qsTr("The sun is the_____ largest object in our solar system"),
-					"options": [qsTr("Sixth"),qsTr("Third"),qsTr("First"),qsTr("Seventh")],
-					"closeness": [qsTr("17.5%"),qsTr("67%"),qsTr("100%"),qsTr("1%")]
+					"question": qsTr("How long does it take the Sun’s light to reach the earth"),
+					"options": [qsTr("8 minutes"),qsTr("30 minutes"),qsTr("60 minutes"),qsTr("15 minutes")],
+					"closeness": ["100%","58.1%","1%","86.6%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("Sun is as big as ____ Earths"),
 					"options": [qsTr("1 million"),qsTr("2.6 million"),qsTr("1.3 million"),qsTr("5 million")],
-					"closeness": [qsTr("92%"),qsTr("65.2%"),qsTr("100%"),qsTr("1%")]
+					"closeness": ["92%","65.2%","100%","1%"]
 				}
 			]
 		},
@@ -38,27 +38,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Mercury is the ______ planet in the Solar System"),
 					"options": [qsTr("Seventh"),qsTr("Sixth"),qsTr("First"),qsTr("Fourth")],
-					"closeness": [qsTr("1%"),qsTr("17.5%"),qsTr("100%"),qsTr("50.5%")]
+					"closeness": ["1%","17.5%","100%","50.5%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("On Mercury, we can survive only for"),
 					"options": [qsTr("2 minutes"),qsTr("4 minutes"),qsTr("1 minute"),qsTr("60 minutes")],
-					"closeness": [qsTr("100%"),qsTr("96.6%"),qsTr("98.2%"),qsTr("1%")]
+					"closeness": ["100%","96.6%","98.2%","1%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("How many moons does Mercury has?"),
-					"options": [qsTr("5"),qsTr("200"),qsTr("0"),qsTr("10")],
-					"closeness": [qsTr("97.5%"),qsTr("1%"),qsTr("100%"),qsTr("95%")]
+					"options": [5,200,0,10],
+					"closeness": ["97.5%","1%","100%","95%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("The maximum temperature on Mercury is"),
 					"options": [qsTr("145 Degrees"),qsTr("35  Degrees"),qsTr("427  Degrees"),qsTr("273  Degrees")],
-					"closeness": [qsTr("28.8%"),qsTr("1%"),qsTr("100%"),qsTr("61.1%")]
+					"closeness": ["28.8%","1%","100%","61.1%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("How many days makes an year on Mercury ?"),
 					"options": [qsTr("365 Days"),qsTr("433 Days"),qsTr("88 Days"),qsTr("107 Days")],
-					"closeness": [qsTr("20.5%"),qsTr("1%"),qsTr("100%"),qsTr("94.5%")]
+					"closeness": ["20.5%","1%","100%","94.5%"]
 				}
 			]
 		},
@@ -69,27 +69,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Venus is the ______ planet in the Solar System."),
 					"options": [qsTr("Seventh"),qsTr("Sixth"),qsTr("Second"),qsTr("Fourth")],
-					"closeness": [qsTr("1%"),qsTr("20.8%"),qsTr("100%"),qsTr("60.4%")]
+					"closeness": ["1%","20.8%","100%","60.4%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("Venus is as heavy as"),
 					"options": [qsTr("0.7 Earths"),qsTr("0.8 Earths "),qsTr("1.3 Earths "),qsTr("2.5 Earths ")],
-					"closeness": [qsTr("94.1%"),qsTr("100%"),qsTr("71%"),qsTr("1%")]
+					"closeness": ["94.1%","100%","71%","1%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("Venus is the _____________ largest planet"),
 					"options": [qsTr("Seventh"),qsTr("Sixth"),qsTr("Fifth"),qsTr("Fourth")],
-					"closeness": [qsTr("50.5%"),qsTr("100%"),qsTr("50.5%"),qsTr("1%")]
+					"closeness": ["50.5%","100%","50.5%","1%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("How long is an year on Venus? "),
 					"options": [qsTr("225 days"),qsTr("365 days"),qsTr("116 days"),qsTr("100 days")],
-					"closeness": [qsTr("100%"),qsTr("1%"),qsTr("23%"),qsTr("11.6%")]
+					"closeness": ["100%","1%","23%","11.6%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("How long is a day on Venus?"),
 					"options": [qsTr("116 days"),qsTr("365 days"),qsTr("88 days"),qsTr("107 days")],
-					"closeness": [qsTr("100%"),qsTr("1%"),qsTr("88.8%"),qsTr("96.4%")]
+					"closeness": ["100%","1%","88.8%","96.4%"]
 				}
 			]
 		},
@@ -100,27 +100,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Earth is the ______ planet in the Solar System"),
 					"options": [qsTr("Sixth"),qsTr("Third"),qsTr("First"),qsTr("Fifth")],
-					"closeness": [qsTr("1%"),qsTr("100%"),qsTr("35%"),qsTr("35%")]
+					"closeness": ["1%","100%","35%","35%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("How long does it take Earth to complete one year?"),
 					"options": [qsTr("200 Days"),qsTr("30 Days"),qsTr("7 Days"),qsTr("365 Days")],
-					"closeness": [qsTr("54.3%"),qsTr("7.3%"),qsTr("1%"),qsTr("100%")]
+					"closeness": ["54.3%","7.3%","1%","100%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("How many moons does Earth has?"),
-					"options": [qsTr("One"),qsTr("Five"),qsTr("Two"),qsTr("Three")],
-					"closeness": [qsTr("100%"),qsTr("15"),qsTr("75.2%"),qsTr("50.5%")]
+					"options": [1,5,2,3],
+					"closeness": ["100%","15%","75.2%","50.5%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("How long is a day on Earth?"),
 					"options": [qsTr("12 Hours"),qsTr("24 Hours"),qsTr("365 Hours"),qsTr("48 Hours")],
-					"closeness": [qsTr("96.5%"),qsTr("100%"),qsTr("1%"),qsTr("93%")]
+					"closeness": ["96.5%","100%","1%","93%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("Earth has ____ seasons."),
-					"options": [qsTr("2"),qsTr("4"),qsTr("6"),qsTr("1")],
-					"closeness": [qsTr("34%"),qsTr("100%"),qsTr("34%"),qsTr("1%")]
+					"options": [2,4,6,1],
+					"closeness": ["34%","100%","34%","1%"]
 				}
 			]
 		},
@@ -131,27 +131,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Mars is the ______ planet in the Solar System"),
 					"options": [qsTr("Sixth"),qsTr("Fourth"),qsTr("First"),qsTr("Fifth")],
-					"closeness": [qsTr("34%"),qsTr("100%"),qsTr("1%"),qsTr("67%")]
+					"closeness": ["34%","100%","1%","67%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("The maximum temperature on Mars is"),
 					"options": [qsTr("20 Degrees"),qsTr("35 Degrees"),qsTr("100 Degrees"),qsTr("0 Degrees")],
-					"closeness": [qsTr("100%"),qsTr("81.4%"),qsTr("1%"),qsTr("75.2%")]
+					"closeness": ["100%","81.4%","1%","75.2%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("Mars is about __________ the size of Earth"),
 					"options": [qsTr("The same"),qsTr("Half"),qsTr("Two times"),qsTr("Three times")],
-					"closeness": [qsTr("80.2%"),qsTr("100%"),qsTr("40.6%"),qsTr("1%")]
+					"closeness": ["80.2%","100%","40.6%","1%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("How many moons does Mars has?"),
-					"options": [qsTr("1"),qsTr("5"),qsTr("2"),qsTr("3")],
-					"closeness": [qsTr("67%"),qsTr("1%"),qsTr("100%"),qsTr("50.5%")]
+					"options": [1,5,2,3],
+					"closeness": ["67%","1%","100%","50.5%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("How long is a day on Mars?"),
 					"options": [qsTr("12 Hours"),qsTr("24 Hours"),qsTr("24.5 Hours"),qsTr("48 Hours")],
-					"closeness": [qsTr("47.3%"),qsTr("90.9%"),qsTr("100%"),qsTr("1%")]
+					"closeness": ["47.3%","90.9%","100%","1%"]
 				}
 			]
 		},
@@ -162,27 +162,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Jupiter is the ______ planet in the Solar System"),
 					"options": [qsTr("Sixth"),qsTr("Fifth"),qsTr("First"),qsTr("Fourth")],
-					"closeness": [qsTr("75.2%"),qsTr("75.2%"),qsTr("1%"),qsTr("100%")]
+					"closeness": ["75.2%","75.2%","1%","100%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("Jupiter is the ______  largest planet in the Solar System"),
 					"options": [qsTr("Third"),qsTr("First"),qsTr("Fifth"),qsTr("Second")],
-					"closeness": [qsTr("50.5%"),qsTr("100%"),qsTr("1%"),qsTr("75.2%")]
+					"closeness": ["50.5%","100%","1%","75.2%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("Jupiter is as big as"),
-					"options": [qsTr("20"),qsTr("100"),qsTr("1000"),qsTr("3000")],
-					"closeness": [qsTr("1%"),qsTr("7.2%"),qsTr("85.5%"),qsTr("100%")]
+					"options": [20,100,1000,3000],
+					"closeness": ["1%","7.2%","85.5%","100%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("How many moons does Jupiter has?"),
-					"options": [qsTr("1"),qsTr("20"),qsTr("25"),qsTr("16")],
-					"closeness": [qsTr("70.1%"),qsTr("92.5%"),qsTr("1%"),qsTr("100%")]
+					"options": [1,20,25,16],
+					"closeness": ["70.1%","92.5%","1%","100%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("How long is one day on Jupiter?"),
 					"options": [qsTr("10 Hours"),qsTr("24 Hours"),qsTr("12 Hours"),qsTr("48 Hours")],
-					"closeness": [qsTr("100%"),qsTr("63.5%"),qsTr("94.8%"),qsTr("1%")]
+					"closeness": ["100%","63.5%","94.8%","1%"]
 				}
 			]
 		},
@@ -193,27 +193,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Saturn is the ______ planet in the Solar System"),
 					"options": [qsTr("Sixth"),qsTr("Fourth"),qsTr("First"),qsTr("Fifth")],
-					"closeness": [qsTr("100%"),qsTr("60.4%"),qsTr("1%"),qsTr("80.2%")]
+					"closeness": ["100%","60.4%","1%","80.2%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("Saturn is the ______ largest  planet in the Solar System"),
 					"options": [qsTr("Third"),qsTr("First"),qsTr("Fifth"),qsTr("Second")],
-					"closeness": [qsTr("67%"),qsTr("67%"),qsTr("1%"),qsTr("100%")]
+					"closeness": ["67%","67%","1%","100%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("How many moons does Saturn has?"),
-					"options": [qsTr("120"),qsTr("1"),qsTr("150"),qsTr("200")],
-					"closeness": [qsTr("80%"),qsTr("1%"),qsTr("100%"),qsTr("60.8%")]
+					"options": [120,1,150,200],
+					"closeness": ["80%","1%","100%","60.8%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("How long is one day on Saturn?"),
 					"options": [qsTr("10.5 Hours"),qsTr("24 Hours"),qsTr("12 Hours"),qsTr("48 Hours")],
-					"closeness": [qsTr("100%"),qsTr("64.3%"),qsTr("96%"),qsTr("1%")]
+					"closeness": ["100%","64.3%","96%","1%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("Saturn is as big as ____ Earths"),
-					"options": [qsTr("100"),qsTr("764"),qsTr("300"),qsTr("1300")],
-					"closeness": [qsTr("1%"),qsTr("100%"),qsTr("30.8%"),qsTr("20.1%")]
+					"options": [100,764,300,1300],
+					"closeness": ["1%","100%","30.8%","20.1%"]
 				}
 			]
 		},
@@ -224,27 +224,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Uranus is the ______ planet in the Solar System"),
 					"options": [qsTr("Seventh"),qsTr("Fourth"),qsTr("Eighth"),qsTr("Fifth")],
-					"closeness": [qsTr("100%"),qsTr("1%"),qsTr("67%"),qsTr("34%")]
+					"closeness": ["100%","1%","67%","34%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("How many years does it take Uranus to go once around the Sun?"),
-					"options": [qsTr("1"),qsTr("24"),qsTr("68"),qsTr("84")],
-					"closeness": [qsTr("1%"),qsTr("28.4%"),qsTr("80.9%"),qsTr("100%")]
+					"options": [1,24,68,84],
+					"closeness": ["1%","28.4%","80.9%","100%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("How many moons does Uranus has?"),
-					"options": [qsTr("120"),qsTr("87"),qsTr("27"),qsTr("50")],
-					"closeness": [qsTr("1"),qsTr("36.1"),qsTr("100"),qsTr("75.5%")]
+					"options": [120,87,27,50],
+					"closeness": ["1%","36.1%","100%","75.5%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("How long is one day on Uranus?"),
 					"options": [qsTr("10 Hours"),qsTr("27 Hours"),qsTr("17 Hours"),qsTr("48 Hours")],
-					"closeness": [qsTr("77.6%"),qsTr("68%"),qsTr("100%"),qsTr("1%")]
+					"closeness": ["77.6%","68%","100%","1%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("Uranus is the ______ largest planet in the Solar System."),
 					"options": [qsTr("Third"),qsTr("First"),qsTr("Seventh"),qsTr("Second")],
-					"closeness": [qsTr("100%"),qsTr("50.5%"),qsTr("1%"),qsTr("75.2%")]
+					"closeness": ["100%","50.5%","1%","75.2%"]
 				}
 			]
 		},
@@ -255,27 +255,27 @@ function get() {
 				{ //sub-level 1
 					"question": qsTr("Neptune is the ______ planet in the Solar System"),
 					"options": [qsTr("Seventh"),qsTr("Fourth"),qsTr("Eighth"),qsTr("Fifth")],
-					"closeness": [qsTr("75.2%"),qsTr("1%"),qsTr("100%"),qsTr("25.7%")]
+					"closeness": ["75.2%","1%","100%","25.7%"]
 				},
 				{ //sub-level 2
 					"question": qsTr("How long does it take Neptune to make one revolution around the Sun?"),
 					"options": [qsTr("165 Years"),qsTr("3 Years"),qsTr("100 Years"),qsTr("1 Years")],
-					"closeness": [qsTr("100%"),qsTr("2.2%"),qsTr("60.7%"),qsTr("1%")]
+					"closeness": ["100%","2.2%","60.7%","1%"]
 				},
 				{ //sub-level 3
 					"question": qsTr("How many moons does Neptune has?"),
-					"options": [qsTr("120"),qsTr("87"),qsTr("14"),qsTr("50")],
-					"closeness": [qsTr("1%"),qsTr("31.8%"),qsTr("100%"),qsTr("66.3%")]
+					"options": [120,87,14,50],
+					"closeness": ["1%","31.8%","100%","66.3%"]
 				},
 				{ //sub-level 4
 					"question": qsTr("How long is one day on Neptune?"),
 					"options": [qsTr("16 Hours"),qsTr("27 Hours"),qsTr("17 Hours"),qsTr("48 Hours")],
-					"closeness": [qsTr("100%"),qsTr("65.9%"),qsTr("96.9%"),qsTr("1%")]
+					"closeness": ["100%","65.9%","96.9%","1%"]
 				},
 				{ //sub-level 5
 					"question": qsTr("The temperature on Neptune is"),
 					"options": [qsTr("100 Degrees"),qsTr("30 Degrees"),qsTr("-214 Degrees"),qsTr("-100 Degrees")],
-					"closeness": [qsTr("1%"),qsTr("23%"),qsTr("100%"),qsTr("64%")]
+					"closeness": ["1%","23%","100%","64%"]
 				}
 			]
 		}
