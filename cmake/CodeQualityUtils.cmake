53bd2fb04bbe6727ef5a7425f9c33b0202e9f78b
Johnny Jazeix <jazeix@gmail.com>
2018-11-11 15:56:39 +0100

    core, use different variables than the ones directly used by cmake

--- cmake/CodeQualityUtils.cmake
+++ cmake/CodeQualityUtils.cmake
@@ -2,8 +2,9 @@
 # Another useful tool could be clazy compiler too
 
 # Run cppcheck on the code
-find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
-if(CMAKE_CXX_CPPCHECK)
+find_program(CPPCHECK_EXE NAMES cppcheck)
+if(CPPCHECK_EXE)
+    set(CMAKE_CXX_CPPCHECK ${CPPCHECK_EXE})
     list(
         APPEND CMAKE_CXX_CPPCHECK 
             "--enable=all"
@@ -11,17 +12,14 @@ if(CMAKE_CXX_CPPCHECK)
             "--force" 
             "--inline-suppr"
     )
-else()
-  unset(CMAKE_CXX_CPPCHECK)
 endif()
 
 # Run clang-tidy
-find_program(CMAKE_CXX_CLANG_TIDY NAMES clang-tidy)
-if(CMAKE_CXX_CLANG_TIDY)
+find_program(CLANG_TIDY_EXE NAMES clang-tidy)
+if(CLANG_TIDY_EXE)
+    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE})
     list(
         APPEND CMAKE_CXX_CLANG_TIDY
             "-checks=*,-fuchsia*,-google*,-hicpp*,-llvm*,-cppcoreguidelines-owning-memory,-modernize-use-auto,-readability-braces-around-statements,-cppcoreguidelines-pro-type-static-cast-downcast,-cppcoreguidelines-pro-type-vararg"
         )
-else()
-  unset(CMAKE_CXX_CLANG_TIDY)
 endif()
