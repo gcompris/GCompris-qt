d4bfc28120048337d1da7fa082a00fad8e6d5619
Timothée Giet <animtim@gmail.com>
2019-02-19 21:09:44 +0100

    tools, add commented workaround in case of too fast animations
    There is a well known recurring issue with nvidia drivers on Linux that can make animations play much faster, when using the default QSG_RENDER_LOOP value.
    Setting it to basic instead of threaded can workaround this issue, but should probably not used as default everywhere.

--- tools/gcompris-qt.sh
+++ tools/gcompris-qt.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=$(dirname $0)
+## uncomment the line below in case of too fast animations, with nvidia drivers especially...
+#export QSG_RENDER_LOOP=basic
 $(dirname $0)/gcompris-qt
 
