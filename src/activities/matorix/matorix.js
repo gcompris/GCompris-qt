755439c79c1f3562769973768d95cdfc26994ffa
Abhay Kaushik <abhay.gyanbharati@gmail.com>
2020-03-22 10:01:25 +0530

    Update copyright

--- src/activities/matorix/matorix.js
+++ src/activities/matorix/matorix.js
@@ -2,9 +2,6 @@
  *
  * Copyright (C) 2020 Abhay Kaushik <abhay.gyanbharati@gmail.com>
  *
- * Authors:
- *   "Abhay Kaushik" <abhay.gyanbharati@gmail.com>
- *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 3 of the License, or
