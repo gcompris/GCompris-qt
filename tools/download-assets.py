d972f8ceccb812d62b18713580d7bcc012f6ca7a
Timothée Giet <animtim@gmail.com>
2020-03-22 20:19:05 +0100

    fix embedded build

--- tools/download-assets.py
+++ tools/download-assets.py
@@ -27,11 +27,11 @@ def download_file(from_url, output_dir):
         shutil.copyfileobj(response, out_file)
 
 # argv[0]: program name
-# argv[1]: assets to download (words, full for full rccs, locale to get correpsonding locale voices)
+# argv[1]: assets to download (words, full for full rccs, music, locale to get corresponding locale voices)
 # argv[2]: audio format (ogg, mp3, aac)
 # argv[3]: output directory (rcc directory)
 if len(sys.argv) != 4:
-    print("Usage: download-assets.py \"words,full,en,fr,pt_BR\" ogg/mp3/aac outputFolder")
+    print("Usage: download-assets.py \"words,full,music,en,fr,pt_BR\" ogg/mp3/aac outputFolder")
     sys.exit(0)
 
 """Download the voices and words assets depending on the wanted audio format
@@ -44,6 +44,7 @@ OUTPUT_FOLDER = sys.argv[3]+"/data2/"
 ALL_LANGUAGES = [x.strip() for x in sys.argv[1].split(",") if len(x)]
 DOWNLOAD_WORDS = "words" in ALL_LANGUAGES
 DOWNLOAD_FULL = "full" in ALL_LANGUAGES
+DOWNLOAD_MUSIC = "music" in ALL_LANGUAGES
 if DOWNLOAD_WORDS:
     ALL_LANGUAGES.remove("words")
     download_file(DOWNLOAD_PATH+"words/words.rcc", OUTPUT_FOLDER+"words/")
@@ -51,6 +52,10 @@ if DOWNLOAD_WORDS:
 if DOWNLOAD_FULL:
     ALL_LANGUAGES.remove("full")
     download_file(DOWNLOAD_PATH+"full-"+AUDIO_FORMAT+".rcc", OUTPUT_FOLDER)
+
+if DOWNLOAD_MUSIC:
+    ALL_LANGUAGES.remove("music")
+    download_file(DOWNLOAD_PATH+"backgroundMusic/backgroundMusic-"+AUDIO_FORMAT+".rcc", OUTPUT_FOLDER+"backgroundMusic/")
 
 for lang in ALL_LANGUAGES:
     lang_url = DOWNLOAD_PATH+"voices-"+AUDIO_FORMAT+"/voices-"+lang+".rcc"
