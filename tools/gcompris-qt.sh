f5afda05f30b7d7be238ec34128262cddceccd89
Johnny Jazeix <jazeix@gmail.com>
2020-03-28 21:09:33 +0100

    tools, pass arguments of gcompris-qt.sh launcher to gcompris

--- tools/gcompris-qt.sh
+++ tools/gcompris-qt.sh
@@ -2,5 +2,4 @@
 export LD_LIBRARY_PATH=$(dirname $0)
 ## uncomment the line below in case of too fast animations, with nvidia drivers especially...
 #export QSG_RENDER_LOOP=basic
-$(dirname $0)/gcompris-qt
-
+$(dirname $0)/gcompris-qt $@
