cc6f117e99aaa2a20a06e6c2994b16b26e07620f
Timothée Giet <animtim@gmail.com>
2022-01-20 10:28:23 +0100

    android, update build scrpt for arm64

--- tools/android_build_all_apk_64.sh
+++ tools/android_build_all_apk_64.sh
@@ -47,7 +47,7 @@ f_cmake()
     fi
 
     cmake -DCMAKE_TOOLCHAIN_FILE=/usr/share/ECM/toolchain/Android.cmake \
-	  -DCMAKE_ANDROID_API=22 \
+	  -DCMAKE_ANDROID_API=21 \
 	  -DCMAKE_BUILD_TYPE=release \
 	  -DANDROID_ARCHITECTURE=$1 \
 	  -DQt5_DIR=${Qt5_BaseDIR}/${QtTarget}/lib/cmake/Qt5 \
