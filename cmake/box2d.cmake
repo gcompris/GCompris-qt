cf460b606159d7c250e5ed4d9c4c6d3158296e93
Johnny Jazeix <jazeix@gmail.com>
2022-06-26 21:47:07 +0200

    box2d: update to cmake commit

--- cmake/box2d.cmake
+++ cmake/box2d.cmake
@@ -17,13 +17,13 @@ if(NOT ${QML_BOX2D_MODULE} STREQUAL "disabled")
     set(_need_box2d_submodule "TRUE")
   else()
     # try to find module in system scope
-    find_library(QML_BOX2D_LIBRARY NAMES Box2D libBox2D PATHS ${_box2d_system_dir} NO_DEFAULT_PATH)
+    find_library(QML_BOX2D_LIBRARY NAMES Box2D libBox2D qmlbox2d PATHS ${_box2d_system_dir} NO_DEFAULT_PATH)
     # Look in default path if not found
     if(NOT QML_BOX2D_LIBRARY AND NOT "${CMAKE_FIND_ROOT_PATH}" STREQUAL "")
       # Remove the root path to look for the library
       set(_box2d_without_cmake_find_root_path)
       string(REPLACE "${CMAKE_FIND_ROOT_PATH}" "" _box2d_without_cmake_find_root_path ${_box2d_system_dir})
-      find_library(QML_BOX2D_LIBRARY NAMES Box2D libBox2D PATHS ${_box2d_without_cmake_find_root_path})
+      find_library(QML_BOX2D_LIBRARY NAMES Box2D libBox2D qmlbox2d PATHS ${_box2d_without_cmake_find_root_path})
     endif()
 
     if(QML_BOX2D_LIBRARY)
@@ -46,22 +46,21 @@ if(NOT ${QML_BOX2D_MODULE} STREQUAL "disabled")
   if(_need_box2d_submodule)
     # build qml-box2d ourselves from submodule
     include(ExternalProject)
-
     get_property(_qmake_program TARGET Qt5::qmake PROPERTY IMPORT_LOCATION)
 
     set(_box2d_source_dir ${CMAKE_CURRENT_SOURCE_DIR}/external/qml-box2d)
     if(WIN32)
-      set(_box2d_library_dir "release/")
-      set(_box2d_library_file "Box2D.dll")
+      set(_box2d_library_dir "bin/plugins/Box2D/")
+      set(_box2d_library_file "qmlbox2d.dll")
     elseif(CMAKE_HOST_APPLE)
-      set(_box2d_library_dir "")
-      set(_box2d_library_file "libBox2D.dylib")
+      set(_box2d_library_dir "bin/plugins/Box2D/")
+      set(_box2d_library_file "libqmlbox2d.dylib")
     elseif(ANDROID AND Qt5Widgets_VERSION VERSION_GREATER_EQUAL "5.14.0")
-      set(_box2d_library_dir "")
-      set(_box2d_library_file "libBox2D_${ANDROID_ABI}.so")
+      set(_box2d_library_dir "bin/plugins/Box2D/")
+      set(_box2d_library_file "libqmlbox2d_${ANDROID_ABI}.so")
     else()
-      set(_box2d_library_dir "")
-      set(_box2d_library_file "libBox2D.so")
+      set(_box2d_library_dir "bin/plugins/Box2D/")
+      set(_box2d_library_file "libqmlbox2d.so")
     endif()
     set(_box2d_install_dir ${CMAKE_CURRENT_BINARY_DIR}/lib/qml/Box2D.2.0)
     # make sure submodule is up2date
@@ -70,39 +69,20 @@ if(NOT ${QML_BOX2D_MODULE} STREQUAL "disabled")
       execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
     endif()
 
-    # for visual studio, we need to create a vcxproj
-    if(WIN32 AND NOT MINGW)
-      set(_qmake_options -spec win32-msvc -tp vc)
-    else()
-      set(_qmake_options "")
-    endif()
-    set(BOX2D_MAKE_PROGRAM ${CMAKE_MAKE_PROGRAM})
-    # Ninja is not supported by qmake.
-    # In case Ninja is set as generator, use make on Linux, nmake on Windows
-    if(${CMAKE_GENERATOR} MATCHES "Ninja")
-      if(WIN32)
-        set(BOX2D_MAKE_PROGRAM "nmake")
-      else()
-        set(BOX2D_MAKE_PROGRAM "make")
+    if(ANDROID)
+      list(APPEND EXTRA_CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DCMAKE_FIND_ROOT_PATH=${CMAKE_FIND_ROOT_PATH} -DANDROID_ABI=${ANDROID_ABI} -DANDROID_PLATFORM=${ANDROID_PLATFORM} -DANDROID_NATIVE_API_LEVEL=${ANDROID_NATIVE_API_LEVEL} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DQML_BOX2D_MODULE=${QML_BOX2D_MODULE} -DQt5_DIR=${Qt5_DIR})
+      if(Qt5Widgets_VERSION VERSION_GREATER_EQUAL "5.14.0")
+        list(APPEND EXTRA_CMAKE_ARGS -DCMAKE_SHARED_LIBRARY_SUFFIX_CXX=_${ANDROID_ABI}.so -DCMAKE_SHARED_MODULE_SUFFIX_CXX=_${ANDROID_ABI}.so -DCMAKE_SHARED_LIBRARY_SUFFIX_C=_${ANDROID_ABI}.so -DCMAKE_SHARED_MODULE_SUFFIX_C=_${ANDROID_ABI}.so)
+        # I didn't find a better way to copy the libraries to the lib folder only on Android when doing an aab package...
+        set(EXTRA_INSTALL_ANDROID_BOX2D ${CMAKE_COMMAND} -E make_directory ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} && ${CMAKE_COMMAND} -E copy ${_box2d_library_dir}${_box2d_library_file} ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} && )
       endif()
-    endif()
-
-    if(ANDROID AND Qt5Widgets_VERSION VERSION_GREATER_EQUAL "5.14.0")
-      # Only build the necessary architecture for box2d
-      # Capitalize first letter of the abi...
-      string(SUBSTRING ${ANDROID_ABI} 0 1 FIRST_LETTER)
-      string(TOUPPER ${FIRST_LETTER} FIRST_LETTER)
-      string(REGEX REPLACE "^.(.*)" "${FIRST_LETTER}\\1" ANDROID_ABI_CAP "${ANDROID_ABI}")
-      set(BOX2D_MAKE_PROGRAM ${BOX2D_MAKE_PROGRAM} -f Makefile.${ANDROID_ABI_CAP})
-      # I didn't find a better way to copy the libraries to the lib folder only on Android when doing an aab package...
-      set(EXTRA_INSTALL_ANDROID_BOX2D ${CMAKE_COMMAND} -E make_directory ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} && ${CMAKE_COMMAND} -E copy ${_box2d_library_dir}${_box2d_library_file} ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} && )
     endif()
 
     ExternalProject_Add(qml_box2d
       DOWNLOAD_COMMAND ""
       SOURCE_DIR ${_box2d_source_dir}
-      CONFIGURE_COMMAND ${_qmake_program} ${_qmake_options} ${_box2d_source_dir}/box2d.pro
-      BUILD_COMMAND ${BOX2D_MAKE_PROGRAM}
+      BUILD_ALWAYS YES
+      CMAKE_ARGS -DUSE_QT6=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} ${EXTRA_CMAKE_ARGS}
       INSTALL_DIR ${_box2d_install_dir}
       INSTALL_COMMAND ${EXTRA_INSTALL_ANDROID_BOX2D} ${CMAKE_COMMAND} -E copy ${_box2d_library_dir}${_box2d_library_file} ${_box2d_source_dir}/qmldir ${_box2d_install_dir}
       )
