e398169f53c65f6311999e28da2fda49fddc82fd
Johnny Jazeix <jazeix@gmail.com>
2022-07-14 23:12:48 +0200

    server, build standalone on Linux

---
+++ tools/gcompris-server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#=============================================================================
+# SPDX-FileCopyrightText: 2022 Johnny Jazeix <jazeix@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#=============================================================================
+export LD_LIBRARY_PATH=$(dirname $0):lib
+## uncomment the line below in case of too fast animations, with nvidia drivers especially...
+#export QSG_RENDER_LOOP=basic
+$(dirname $0)/gcompris-server $@
