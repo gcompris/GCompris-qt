1625336c180b007c3565040fe936b9730bef86bf
Johnny Jazeix <jazeix@gmail.com>
2023-01-28 15:03:01 +0100

    core, fix sound playing with Qt6

--- src/core/synth/GSynth.h
+++ src/core/synth/GSynth.h
@@ -11,8 +11,8 @@
 #ifndef GSYNTH_H
 #define GSYNTH_H
 
-#include <QAudioOutput>
 #include <QAudioFormat>
+#include <QAudioSink>
 #include <QTimer>
 #include <QQmlEngine>
 #include "preset.h"
@@ -55,7 +55,7 @@ private:
     Generator        *m_generator;
     QAudioFormat      m_format;
     QByteArray        m_buffer;
-    QAudioOutput     *m_audioOutput;
+    QAudioSink       *m_audioSink;
     QMap<int, QTimer *> m_timers;
     
     Preset PresetCustom;
