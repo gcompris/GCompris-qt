68e4e328ce7cf385df99e7dc4229d92751c7c8d9
Johnny Jazeix <jazeix@gmail.com>
2023-05-19 15:00:25 +0200

    reuse, add missing licences and create a .reuse/dep5 file to list all the licences per file

--- tools/inkscape/convertSvgToSvgz.sh
+++ tools/inkscape/convertSvgToSvgz.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#=============================================================================
+# SPDX-FileCopyrightText: 2021 Timothée Giet <animtim@gmail.com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#=============================================================================
+#
 # Convert all svg files in a directory to plain svgz
 # needed to compress the output of export_layers_compris inkscape extension,
 # as trying to export directly svgz from it seems to break the --export-area-drawing option
