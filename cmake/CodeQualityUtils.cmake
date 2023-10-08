8111da762d20e1df5fbede167d1ca2c4026b775e
Johnny Jazeix <jazeix@gmail.com>
2023-10-08 21:24:10 +0200

    core, ignore misc-include-cleaner rule in clang-tidy

--- cmake/CodeQualityUtils.cmake
+++ cmake/CodeQualityUtils.cmake
@@ -25,6 +25,6 @@ if(CLANG_TIDY_EXE)
     set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE})
     list(
         APPEND CMAKE_CXX_CLANG_TIDY
-            "-checks=*,-fuchsia*,-google*,-hicpp*,-llvm*,-cppcoreguidelines-*,-modernize-use-auto,-modernize-use-trailing-return-type,-readability-*,-altera-*,-misc-const-correctness,-bugprone-easily-swappable-parameters,-modernize-return-braced-init-list"
+            "-checks=*,-fuchsia*,-google*,-hicpp*,-llvm*,-cppcoreguidelines-*,-modernize-use-auto,-modernize-use-trailing-return-type,-readability-*,-altera-*,-misc-const-correctness,-bugprone-easily-swappable-parameters,-modernize-return-braced-init-list,-misc-include-cleaner"
         )
 endif()
