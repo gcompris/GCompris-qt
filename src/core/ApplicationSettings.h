cea5c432b68143199c2cb0cc5bbff31bc990aa08
Johnny Jazeix <jazeix@gmail.com>
2023-11-25 21:46:57 +0100

    server+core, make code compilable after rebase emit -> Q_EMIT signals -> Q_SIGNALS slots -> Q_SLOTS protobuf seems to need c++17 to work. We probably need to use an older version to keep C++11 compatibilty (or do we want to bump to c++17) workaround needed with it to link with abseil

--- src/core/ApplicationSettings.h
+++ src/core/ApplicationSettings.h
@@ -445,7 +445,7 @@ public:
     void setDeviceId(const QString &newDeviceId)
     {
         m_deviceId = newDeviceId;
-        emit deviceIdChanged();
+        Q_EMIT deviceIdChanged();
     }
 
     QString cachePath() const { return m_cachePath; }
