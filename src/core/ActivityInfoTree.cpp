4f1864f2f45d0bcb02cfa43a040e09283bd34a3a
Johnny Jazeix <jazeix@gmail.com>
2025-02-09 16:05:49 +0100

    core, fix merge issue with duplication of activities

--- src/core/ActivityInfoTree.cpp
+++ src/core/ActivityInfoTree.cpp
@@ -351,7 +351,6 @@ ActivityInfoTree *ActivityInfoTree::create(QQmlEngine *engine, QJSEngine *script
     Q_UNUSED(scriptEngine)
 
     ActivityInfoTree *menuTree = getInstance();
-    menuTree->initialize(engine);
     return menuTree;
 }
 
