a5f49be6c6072a045a9070ea708244ec79bef798
Johnny Jazeix <jazeix@gmail.com>
2025-09-15 10:31:54 +0200

    gitlab, run cppcheck by the CI

--- cmake/CodeQualityUtils.cmake
+++ cmake/CodeQualityUtils.cmake
@@ -6,25 +6,12 @@
 # These tools are run (for now) only if BUILD_TESTING is ON
 # Another useful tool could be clazy compiler too
 
-# Run cppcheck on the code
-find_program(CPPCHECK_EXE NAMES cppcheck)
-if(CPPCHECK_EXE)
-    set(CMAKE_CXX_CPPCHECK ${CPPCHECK_EXE})
-    list(
-        APPEND CMAKE_CXX_CPPCHECK
-            "--enable=all"
-            "--inconclusive"
-            "--force"
-            "--inline-suppr"
-    )
-endif()
-
 # Run clang-tidy
 find_program(CLANG_TIDY_EXE NAMES clang-tidy)
 if(CLANG_TIDY_EXE)
     set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE})
     list(
         APPEND CMAKE_CXX_CLANG_TIDY
-            "-checks=*,-fuchsia*,-google*,-hicpp*,-llvm*,-cppcoreguidelines-*,-modernize-use-auto,-modernize-use-trailing-return-type,-readability-*,-altera-*,-misc-const-correctness,-bugprone-easily-swappable-parameters,-modernize-return-braced-init-list,-misc-include-cleaner"
+            "-checks=*,-fuchsia*,-google*,-hicpp*,-llvm*,-cppcoreguidelines-*,-modernize-use-auto,-modernize-use-trailing-return-type,-readability-*,-altera-*,-misc-const-correctness,-bugprone-easily-swappable-parameters,-modernize-return-braced-init-list,-misc-include-cleaner,-boost*"
         )
 endif()
