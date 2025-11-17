b08b0a102d0ed1a2e63aac712d91cc1f3278307c
Johnny Jazeix <jazeix@gmail.com>
2025-11-17 11:29:15 +0100

    core, correct extractions for strings

--- src/Messages.sh
+++ src/Messages.sh
@@ -7,14 +7,14 @@
 # Extract strings from all source files.
 # EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
 # lconvert.
-$EXTRACT_TR_STRINGS `find . \
+$EXTRACT_TR_STRINGS `find activities/ core/ \
                           -name \*.cpp -o \
                           -name \*.h -o \
-                          -name \*.qml -not -path "./activities/template/*" -o \
-                          -name \*.js -not -path "./activities/template/*"` \
+                          -name \*.qml -not -path "activities/template/*" -o \
+                          -name \*.js -not -path "activities/template/*"` \
                     -o $podir/gcompris_qt.pot
 
-$EXTRACT_TR_STRINGS `find server/ -name \*.cpp -o -name \*.h -o -name \*.qml -o -name \*.js` -o $podir/gcompris_server.pot
+$EXTRACT_TR_STRINGS `find server/ -name \*.cpp -o -name \*.h -o -name \*.qml -o -name \*.js` -o $podir/gcompris_teachers.pot
 
 # create a pot for the voices text to keep them up-to-date with the audio files
 find . -name ActivityInfo.qml -not -path "*template*" -exec awk -f activityintro2msg.awk {} \; > rc.cpp
