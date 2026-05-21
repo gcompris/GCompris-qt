0b22c8d541b06cffa0e8861dad3e129551a1e2d4
Timothée Giet <animtim@gmail.com>
2026-05-21 15:56:22 +0200

    server, fix typo in netLog

--- src/server/controllers/network-controller.cpp
+++ src/server/controllers/network-controller.cpp
@@ -285,7 +285,7 @@ namespace controllers {
                 count++;
             }
         }
-        sendNetLog(QString("Login list sended to %1 users").arg(count));
+        sendNetLog(QString("Login list sent to %1 users").arg(count));
     }
 
     void NetworkController::sendDatasetToUsers(const QJsonValue &dataset_content, const QList<int> &selectedUsersId)
